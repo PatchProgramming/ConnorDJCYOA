24284bb429b91ce399bd726bb8b5626783c8f7cf
Hoopes.Connor <84668903+PatchProgramming@users.noreply.github.com>
2023-12-06 01:06:08 +0000

    additional logic

--- cyoa.py
+++ cyoa.py
@@ -2,28 +2,51 @@
 #coin flip on bridge
 #
 #Modules
-import math
 import random
-
+import time
 #Game Start function
 directions = ["Left","Right","Back","Forward"]
-def Start():
+def Start(): #main function which calls all others
     print("You awake on a path in a forest, you look around to see your surroundings.")
     print("Looking around you can see there is a bridge to your LEFT. \nTo your RIGHT, there is what seems to be an abandoned mine.")
     print("Which way do you want to go? \n LEFT \n RIGHT")
     Decision_One()
 def Decision_One():
-    choice = input()
+    choice = input("LEFT or RIGHT")
     #choice right
     if choice.upper() == "RIGHT":
-        print("You walk up to the abandoned mine and see a sign that says \"Danger Keep Out\" \n would you like to \n[1]Enter the Mine \n[2]Take the path to the bridge")
+        mineDecision()
     elif choice.upper() == "LEFT":
-        print("Walking along the path you come to a bridge. \n Looking around you can see a tall and imposing troll standing on the bridge continully flipping a flipping a large disc to him self. \n To the left you can see")
+        pathDecision()
+    elif choice.upper() == "EXIT()":
+        exit()
+    else:
+        print("Adventurer I didn't catch that... do you want to go LEFT or RIGHT?")
+        Decision_One()
+def mineDecision():
+    #if else statement for options
+    print("""\nYou walk up to the abandoned mine and see a sign that says \"Danger Keep Out\".
+    would you like to: 
+    [1]:Enter the Mine
+    [2]:Take the path to the bridge
+    [3]:Go back and follow the path to the bridge""")
+    time.sleep(3)   
+    choice = input("Enter a number to make your choice")
+
+#funtion for logic of the Path decision
+def pathDecision():
+    print("\nWalking along the path you come to a bridge. \nLooking around you can see a tall and imposing troll standing on the bridge continully flipping a flipping a large disc to himself. \n To the left you can see what looks like an old rope swing.\nThere is a rushing River beneath both which you don't thing you can swim across")
+    print("""Would you like to:
+    [1]:cross the bridge and face the troll
+    [2]:go the the left and inspect the rope swing
+    [3]:try swimming across
+    [4]:Go back to the mine""")
+    time.sleep(5)
+    choice = input("Enter a number to make your choice")
+    #if else statement for options
 #random chance generator
 def chance(range):
     return random.randrange(range)
 
-#
-print("Walking along the path you come to a bridge. \n Looking around you can see a tall and imposing troll standing on the bridge continully flipping a flipping a large disc to him self. \n To the left you can see")
-
-# Start()
+#game start
+Start()
